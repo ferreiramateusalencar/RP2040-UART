6df363b6d5a42a3c78d97980cd945a476f6e1fca
ferreiramateusalencar <ferreiramateusalencar@gmail.com>
2025-02-08 19:44:42 -0300

    second commit

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
 #include "include/font.h"
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -34,8 +35,8 @@ int main()
     ssd1306_fill(&ssd, !cor); // Limpa o display
     ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
     ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
-    ssd1306_draw_string(&ssd, "Mateus Alencar Ferreira", 15, 48); // Desenha uma string      
+    ssd1306_draw_string(&ssd, "MATEUS A. FERREIRA", 8, 30); // Desenha uma string
+    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 48); // Desenha uma string      
     ssd1306_send_data(&ssd); // Atualiza o display
 
     sleep_ms(1000);
